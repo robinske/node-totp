553a0c1c1c4601c036b59be4a08d20831486fa1b
Kelley Robinson <krobinson@twilio.com>
2019-03-20 16:22:02 -0400

    prettier

--- totp.js
+++ totp.js
@@ -1,27 +1,21 @@
-const speakeasy = require('speakeasy');
-const opn = require('opn');
-const qrcode = require('qrcode');
-const fs = require('fs');
+const speakeasy = require("speakeasy");
+const opn = require("opn");
+const qrcode = require("qrcode");
+const fs = require("fs");
 
-
-var secret = speakeasy.generateSecret({name: 'brooklyn.js demo'});
-
+var secret = speakeasy.generateSecret({ name: "brooklyn.js demo" });
 
 function generateQRcode() {
-    qrcode.toDataURL(secret.otpauth_url, function(err, data) {
-        fs.writeFileSync('./qr.html', `<img src="${data}">`);
-        opn("./qr.html");
-    });
+  qrcode.toDataURL(secret.otpauth_url, function(err, data) {
+    fs.writeFileSync("./qr.html", `<img src="${data}">`);
+    opn("./qr.html");
+  });
 }
-
 
 function verifyToken(token) {
-    return speakeasy.totp.verify({
-        secret: secret.base32,
-        encoding: 'base32',
-        token: token
-    });
+  return speakeasy.totp.verify({
+    secret: secret.base32,
+    encoding: "base32",
+    token: token
+  });
 }
-
-
-
